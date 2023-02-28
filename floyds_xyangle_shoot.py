592eab60bbb8fd0cbd3256bebc24d7577f68f58e
Prerana Kottapalli <pkottapalli@intern06-linux.lco.gtn>
2023-02-28 15:08:34 -0800

    changed date formatting on plots of header values

--- floyds_xyangle_shoot.py
+++ floyds_xyangle_shoot.py
@@ -33,8 +33,8 @@ import matplotlib.dates as mdates
 from datetime import datetime
 
 header_values = ['AZIMUTH', 'ALTITUDE', 'ROTANGLE']
-headers = [fits.open(f)[0].header for f in files]
-sorting_idx = np.argsort([header['UTSTART'] for header in headers])
+headers = [fits.open(f)['SCI'].header for f in files]
+sorting_idx = np.argsort([header['DATE-OBS'] for header in headers])
 
 env_parameters = []
 for header in headers:
@@ -45,17 +45,18 @@ env_parameters = np.array(env_parameters)
 #plt.style.use('seaborn')
 fig, axes = plt.subplots(1, 3, sharex=True, figsize=(35,10))
 fig.tight_layout(pad = 12)
-times = np.sort([header['DATE-OBS'] for header in headers])
-t0 = datetime.strptime(times[0], '%Y-%m-%dT%H:%M:%S.%f')
-t1 = datetime.strptime(times[-1], '%Y-%m-%dT%H:%M:%S.%f')
+times = np.sort([datetime.strptime(header['DATE-OBS'], '%Y-%m-%dT%H:%M:%S.%f') for header in headers])
+t0 = times[0]
+t1 = times[-1]
 for ax, i in zip(axes.flatten(), range(len(env_parameters[0]))):
     ax.scatter(times, env_parameters[:,i][sorting_idx], s=15)
     ax.set_ylabel(header_values[i], fontsize=20)
     ax.set_xlabel('Obs time', fontsize=20)
     ax.tick_params(axis='both', which='major', labelsize=14, size=10)
-    #locator = mdates.DateLocator()
-    #ax.xaxis.set_major_locator(locator)
-    #ax.xaxis.set_minor_locator(locator)
+    locator = mdates.AutoDateLocator()
+    formatter = mdates.ConciseDateFormatter(locator)
+    ax.xaxis.set_major_locator(locator)
+    #ax.xaxis.set_major_formatter(formatter)
     plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
 fig.show()
 #%%
