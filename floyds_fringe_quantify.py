d6d8285270ec62593c27e5d2ea87ba128a57245f
Prerana Kottapalli <pkottapalli@intern06-linux.lco.gtn>
2023-02-28 15:09:01 -0800

    added code to shift around the images until fringing is minimized.

--- floyds_fringe_quantify.py
+++ floyds_fringe_quantify.py
@@ -12,12 +12,12 @@ from glob import glob
 from scipy.fft import fft, fftfreq
 import numpy.ma as ma
 #%%
-files = sorted(glob('lco_data-20230216-33/lco_data-20230216-33/flat*red_*.fits', recursive=True))
-#files = sorted(glob('New_AltAz_data/ogg2m001-*w00.fits.fz', recursive=True))
-images = [fits.open(f)[0].data for f in files]
+#files = sorted(glob('lco_data-20230216-33/lco_data-20230216-33/flat*red_*.fits', recursive=True))
+files = sorted(glob('New_AltAz_data/ogg2m001-*w00.fits.fz', recursive=True))
+images = [fits.open(f)['SCI'].data for f in files]
 data_type = 'red flat'
 #%% Select a subregion of this non-rectified flat
-for i, im in enumerate(images):
+for i, im in enumerate(images[:10]):
     plt.imshow(im[175:225, 1200:1500], origin='lower')
     plt.colorbar()
     plt.title(f'{data_type}, {i} fringe region')
@@ -25,9 +25,9 @@ for i, im in enumerate(images):
 #Now look at the linecut
 fringe_rms = []
 for i, im in enumerate(images):
-    plt.plot(np.arange(300), im[200, 1200:1500])
-    plt.title(f'{data_type}, {i} fringe region linecut')
-    plt.show()
+    # plt.plot(np.arange(300), im[200, 1200:1500])
+    # plt.title(f'{data_type}, {i} fringe region linecut')
+    # plt.show()
     fringe_rms.append(np.std(im[200, 1200:1500]))
 #%% Plot std with image number
 plt.figure(dpi=200)
@@ -50,12 +50,12 @@ for image in images:
     masked_freqs = ma.masked_inside(xf,-0.015, 0.015)
     frequency = np.abs(xf[~masked_freqs.mask])
     power = np.abs(yf[~masked_freqs.mask])
-    plt.title(f'Power Spectrum of the {data_type}')
-    plt.plot(frequency, power)
-    plt.xlim(0,0.1)
-    plt.xlabel('Frequency')
-    plt.ylabel('Power')
-    plt.show()
+    # plt.title(f'Power Spectrum of the {data_type}')
+    # plt.plot(frequency, power)
+    # plt.xlim(0,0.1)
+    # plt.xlabel('Frequency')
+    # plt.ylabel('Power')
+    # plt.show()
     
     fringe_freq.append(frequency[np.argmax(power)])
     fringe_power.append(np.max(power))
@@ -116,28 +116,32 @@ def power_fringe(im):
     return frequency[np.argmax(power)], np.max(power)
 
 
-divim_fringe_power, divim_fringe_freq = power_fringe(div_im)
+divim_fringe_freq, divim_fringe_power = power_fringe(div_im)
+print(div_im_std, divim_fringe_power)
 #%% Shift the images in xy
 im_new = xy_shift(images[im1_num], 1, 0, x_up=False, y_up=True)
 div_im = np.divide(im_new,images[im2_num])
 
 shifted_fringing_std = std_fringe(div_im)
-shifted_fringing_power, shifted_fringing_freq = power_fringe(div_im)
+shifted_fringing_freq, shifted_fringing_power = power_fringe(div_im)
+print(shifted_fringing_std, shifted_fringing_power)
 #%% Rotate the images
 im_new = rotate(images[0], 1, True)
 div_im = np.divide(im_new,images[im2_num])
 rotated_fringing_std = std_fringe(div_im)
-rotated_fringing_power, rotated_fringing_freq = power_fringe(div_im)
+rotated_fringing_freq, rotated_fringing_power = power_fringe(div_im)
+print(rotated_fringing_std, rotated_fringing_power)
 #%% Shift AND Rotate
 im_new = xy_shift(images[im1_num], 1, 0, x_up=True, y_up=True)
 im_new = rotate(im_new, 0.01, True)
 div_im = np.divide(im_new,images[im2_num])
 
 new_fringing_std = std_fringe(div_im)
-new_fringing_power, rotated_fringing_freq = power_fringe(div_im)
+new_fringing_freq, new_fringing_power = power_fringe(div_im)
+print(new_fringing_std, new_fringing_power)
 #%% Make a map of fringing based on altitude-rotation coordinates
 header_values = ['AZIMUTH', 'ALTITUDE', 'ROTANGLE']
-headers = [fits.open(f)[0].header for f in files]
+headers = [fits.open(f)['SCI'].header for f in files]
 sorting_idx = np.argsort([header['UTSTART'] for header in headers])
 
 altitude = [header['ALTITUDE'] for header in headers]
@@ -145,7 +149,7 @@ azimuth = [header['AZIMUTH'] for header in headers]
 rotangle = [header['ROTANGLE'] for header in headers]
 #%% altitude-rotangle plot
 fig, ax = plt.subplots(dpi=200, subplot_kw={'projection':'polar'}, figsize=(10,10))
-plot = ax.scatter(np.array(rotangle)*np.pi/180, altitude, c = fringe_rms, alpha=0.8)
+plot = ax.scatter(np.array(rotangle)*np.pi/180, altitude, c = fringe_rms, s=7, alpha=0.8)
 ax.set_title(r'Fringe $\sigma$ with altitude-rotangle', fontsize=20)
 ax.set_xlabel(r'Altitude ($\degree$)', labelpad=5, fontsize=20)
 #---raxis formatting---
@@ -169,7 +173,7 @@ cbar.ax.set_ylabel(r'Fringing $\sigma$', rotation=270, labelpad=25, fontsize=16)
 plt.show()
 #%% rotangle-azimuth plot
 fig, ax = plt.subplots(dpi=200, subplot_kw={'projection':'polar'}, figsize=(10,10))
-plot = ax.scatter(np.array(rotangle)*np.pi/180, azimuth, c = fringe_rms, alpha=0.8)
+plot = ax.scatter(np.array(rotangle)*np.pi/180, azimuth, c = fringe_rms, s=7, alpha=0.8)
 ax.set_title(r'Fringe $\sigma$ with rotangle-azimuth', fontsize=20)
 ax.set_xlabel(r'Azimuth ($\degree$)', labelpad=5, fontsize=20)
 #ax.set_rgrids((20,40,60,80))
@@ -181,3 +185,54 @@ ax.set_thetagrids((30, 35, 40))
 cbar = fig.colorbar(plot)
 cbar.ax.set_ylabel(r'Fringing $\sigma$', rotation=270, labelpad=25, fontsize=16)
 plt.show()
+#%% shift in x and y until fringing is minimized, then map out shift on sky
+
+#Shift in every direction by 1 pixel
+shift_num = 2
+
+shift_dict = {'north': xy_shift(images[im1_num], 0, shift_num, y_up=True), 
+              'northeast': xy_shift(images[im1_num],shift_num,shift_num, x_up=True, y_up=True), 
+              'east': xy_shift(images[im1_num],shift_num, 0, x_up=True), 
+              'southeast': xy_shift(images[im1_num],shift_num,shift_num, x_up=True, y_up=False),
+              'south': xy_shift(images[im1_num], 0,shift_num, y_up=False),
+              'southwest': xy_shift(images[im1_num],shift_num,shift_num, x_up=False, y_up=False),
+              'west': xy_shift(images[im1_num],shift_num, 0, x_up=False),
+              'northwest': xy_shift(images[im1_num],shift_num,shift_num, x_up=False, y_up=True), 
+              'none': images[im1_num]}
+
+alt1 = altitude[0]
+az1 = azimuth[0]
+rot1 = rotangle[0]
+#Shift every image relative to the first image, in every direction
+best_shift = []
+for i in range(len(images[:5])):
+    im1_num = i
+    alt2 = altitude[i]
+    az2 = azimuth[i]
+    rot2 = rotangle[i]
+    fringe_amount = []
+    for shift in shift_dict:
+        shifted_image = shift_dict[shift]
+        div_im = np.divide(images[0], shifted_image)
+        fringe_amount.append(std_fringe(div_im))
+        #Plot fringe section
+        plt.imshow(div_im[175:225, 1200:1500], origin='lower')
+        plt.colorbar()
+        plt.title(f'{data_type}, {i} fringe region')
+        plt.show()
+        #Plot linecut
+        plt.plot(np.arange(300), div_im[200, 1200:1500])
+        plt.title(f'{data_type}, {i} fringe region linecut')
+        plt.xlabel('x pixel')
+        plt.ylabel('pixel value')
+        plt.show()
+        #Plot divided images
+        median = np.median(div_im)
+        std = np.std(div_im)
+        fig = plt.figure(figsize=(7.5,5), dpi=200)
+        plt.imshow(div_im, origin='lower', vmin=median-2*std, vmax = median+2*std)
+        plt.title(f'Two flats divided, shifted {shift}')
+        plt.xlabel(f'ALTAZROT1 : ({alt1:.2f}, {az1:.2f}, {rot1:0.2f}), ALTAZROT2 : ({alt2:.2f}, {az2:.2f}, {rot2:0.2f})')
+        plt.colorbar()
+        plt.show()
+    best_shift.append(list(shift_dict)[np.argmin(fringe_amount)])
