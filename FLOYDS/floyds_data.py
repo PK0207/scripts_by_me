d1ee732fc8b459946ea68e1fbe945c45fe4e5dd4
Prerana Kottapalli <pkottapalli@intern06-linux.lco.gtn>
2023-03-16 17:06:19 -0700

    attempting to get more data, very slow and doesn't work for all files

--- FLOYDS/floyds_data.py
+++ FLOYDS/floyds_data.py
@@ -19,6 +19,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from urllib.parse import urljoin
+import os
 #%% Authenticate
 requests.post(
     'https://observe.lco.global/api/api-token-auth/',
@@ -27,47 +28,61 @@ requests.post(
         'password': '&2m9X<n2'
     }
 ).json()
-
 #%%
-def _get_observations_offset(query_params: dict):
-    observations_endpoint = urljoin('https://archive-api.lco.global', 'frames')
-    try:
-        url = requests.Request('GET', observations_endpoint, params=query_params).prepare().url
-        response = requests.get(url, headers={'Authorization': 'Token 9b605ef2229d317ba1b031b54f2a0115aec69b9f'})
-        response.raise_for_status()
-    except requests.HTTPError:
-        logging.warning("observations endpoint {} failed with status code {}: {}".format(response.url,
-                                                                                         response.status_code,
-                                                                                         response.text))
-        return []
-    return response.json()
-def get_observations(query_params):
-    query_params['offset'] = 0
-    response = _get_observations_offset(query_params)
-    observations = response['results']
-    while response['count'] > len(observations):
-        query_params['offset'] = len(observations)
-        response = _get_observations_offset(query_params)
-        observations.extend(response['results'])
+import csv
+csv_path = '/home/pkottapalli/Downloads/On_demand_report_2023-03-16T17 02 56.539Z_65dbdeb0-c41c-11ed-8948-cb80406fa13a.csv'
+filenames = []
+with open(csv_path, newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for row in spamreader:
+        filename = row[0]
+        filenames.append(filename)
+filenames=sorted(filenames)
+#%%
+# def _get_observations_offset(query_params: dict):
+#     observations_endpoint = urljoin('https://archive-api.lco.global', 'frames')
+#     try:
+#         url = requests.Request('GET', observations_endpoint, params=query_params).prepare().url
+#         response = requests.get(url, headers={'Authorization': 'Token 9b605ef2229d317ba1b031b54f2a0115aec69b9f'})
+#         response.raise_for_status()
+#     except requests.HTTPError:
+#         logging.warning("observations endpoint {} failed with status code {}: {}".format(response.url,
+#                                                                                          response.status_code,
+#                                                                                          response.text))
+#         return []
+#     return response.json()
+# def get_observations(query_params):
+#     query_params['offset'] = 0
+#     response = _get_observations_offset(query_params)
+#     observations = response['results']
+#     while response['count'] > len(observations):
+#         query_params['offset'] = len(observations)
+#         response = _get_observations_offset(query_params)
+#         observations.extend(response['results'])
 
-    return observations
-query_params={'offset':0}#, 'instrument_id':'en06', 'site_id':'ogg', 'start':'2022-01-01', 'end':'2023-01-01', 'configuration_type':'LAMPFLAT', 'public':True}
+#     return observations
+
+#query_params={'offset':0}#, 'instrument_id':'en06', 'site_id':'ogg', 'start':'2022-01-01', 'end':'2023-01-01', 'configuration_type':'LAMPFLAT', 'public':True}
 #Get all headers from a year
 altitude = []
 azimuth = []
 rotangle = []
 frameid = []
-archive_record = requests.get('https://archive-api.lco.global/frames/?instrument_id=en06&site_id=ogg&start=2022-01-01&end=2023-01-01&configuration_type=LAMPFLAT&public=True&limit=1000', headers={'Authorization': 'Token 9b605ef2229d317ba1b031b54f2a0115aec69b9f'}).json()
-#while archive_record['count'] > len(altitude):
-for rec in archive_record['results']:    
-    query_params['offset'] = len(altitude)
-    #response = _get_observations_offset(query_params)
-    frame_id = rec["id"]
-    frameid.append(frame_id)
-    header = requests.get(f'https://archive-api.lco.global/frames/{frame_id}/headers/', headers={'Authorization': 'Token 9b605ef2229d317ba1b031b54f2a0115aec69b9f'}).json()
-    altitude.append(header['data']['ALTITUDE'])
-    azimuth.append(header['data']['AZIMUTH'])
-    rotangle.append(header['data']['ROTANGLE'])
+for f in filenames[1:]:
+    filename = os.path.splitext(os.path.splitext(f)[0])[0]
+    print(filename)
+    archive_record = requests.get(f'https://archive-api.lco.global/frames/?site_id=ogg&basename={filename}&start=2022-01-01 00%3A00&end=2023-01-01 00%3A00&public=true&limit=1000', headers={'Authorization': 'Token 9b605ef2229d317ba1b031b54f2a0115aec69b9f'}).json()
+    #while archive_record['count'] > len(altitude):
+    if len(archive_record['results']) > 0:
+        rec = archive_record['results'][0]
+        #query_params['offset'] = len(altitude)
+        #response = _get_observations_offset(query_params)
+        frame_id = rec["id"]
+        frameid.append(frame_id)
+        header = requests.get(f'https://archive-api.lco.global/frames/{frame_id}/headers/', headers={'Authorization': 'Token 9b605ef2229d317ba1b031b54f2a0115aec69b9f'}).json()
+        altitude.append(header['data']['ALTITUDE'])
+        azimuth.append(header['data']['AZIMUTH'])
+        rotangle.append(header['data']['ROTANGLE'])
 df = pd.DataFrame({'Altitude':altitude, 'Azimuth':azimuth, 'Rotangle': rotangle}, index=frameid)
 #%% Plot the locations of each coordinate
 plt.figure(dpi=200)
