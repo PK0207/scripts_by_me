626602d885f617c6b22096965592186115b3adbe
Prerana Kottapalli <prerana0207@gmail.com>
2026-04-10 12:34:36 -0400

    Revert "ML class code/data in one filder"
    This reverts commit 3d3408ecedb635be8eddea52d4a3ea22c4167811.

--- research24/sim_fitting_code/merge_script.py
+++
@@ -1,8 +0,0 @@
-from glob import glob
-import pandas as pd
-import numpy as np
-
-all_files = sorted(glob("./results/chunk_*.pkl"))
-merged = pd.concat([pd.read_pickle(f) for f in all_files]).sort_index()
-merged.to_pickle("./results/sims_fitted.pkl")
-print(f"Done. {len(merged)} rows saved to sims_fitted.pkl")
